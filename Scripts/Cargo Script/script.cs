772b33725ffc5dcc186e345f6dcaa1a63ada1d83
Teuschl Nikolas <niklas.teuschl@hotmail.com>
2020-09-27 00:53:52 +0200

    Updated and refined cargo scripts

--- Scripts/Cargo Script/script.cs
+++ Scripts/Cargo Script/script.cs
@@ -21,71 +21,49 @@ namespace SpaceEngineers.UWBlockPrograms.CargoScript {
     public sealed class Program : MyGridProgram {
 #endregion
 // YOUR CODE BEGIN
-﻿public void Main(string argument, UpdateType updateSource) {
-    if (argument == "") {
-       argument = "0";
-   }
-    setupCockpit(Convert.ToInt32(argument),  1.5f, Color.Orange);
+public Program() {
+    Runtime.UpdateFrequency = UpdateFrequency.Update100;
+}
 
-    List<IMyFunctionalBlock> blocks = new List<IMyFunctionalBlock>();
-    GridTerminalSystem.GetBlocksOfType<IMyFunctionalBlock>(blocks);
+string persistentArg;
+﻿public void Main(string argument, UpdateType updateSource) {
+    List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
+    GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(blocks);
+
+    if (argument != "") {
+        persistentArg = argument;
+    }
+
+    List<IMyTextPanel> panels = new List<IMyTextPanel>();
+    IMyBlockGroup panelGroup = GridTerminalSystem.GetBlockGroupWithName("CPanels");
+    panelGroup.GetBlocksOfType<IMyTextPanel>(panels);
 
     float tempVal;
     string printString = "";
     float maxInv = 0;
     float curInv = 0;
-    foreach (IMyFunctionalBlock block in blocks) {
-        if (!block.HasInventory || block is IMyReactor || !block.ShowInInventory) continue;
+    foreach (IMyTerminalBlock block in blocks) {
+        if (!block.HasInventory || block is IMyReactor || !block.ShowInInventory || block.CubeGrid.CustomName != persistentArg) continue;
 
         IMyInventory inv = block.GetInventory();
         maxInv += (float)inv.MaxVolume;
         curInv += (float)inv.CurrentVolume;
         tempVal = (float)inv.CurrentVolume * 100 / (float)inv.MaxVolume;
-        printString += block.CustomName + ": " + Convert.ToInt32(tempVal) + "%\n";
+        printString += $"{block.CustomName}: {tempVal:F1}%\n";
     }
 
-   int fillRatio = (int)(curInv * 100 / maxInv);
-   printString = "Fill Ratio: " + fillRatio + "%\n" + "----------\n" + printString;
-   writeToCockpit(Convert.ToInt32(argument), printString);
-   Runtime.UpdateFrequency = UpdateFrequency.Update100;
+    int fillRatio = (int)(curInv * 100 / maxInv);
+    printString = $"Total Ratio: {fillRatio}%\n----------\n{printString}";
+    foreach (IMyTextPanel panel in panels) {
+        if (panel.CubeGrid.CustomName != persistentArg) continue;
+
+        panel.ContentType = ContentType.TEXT_AND_IMAGE;
+        panel.FontSize = 1.0f;
+        panel.FontColor = Color.Orange;
+        panel.WriteText(printString);
+    }
 }
 
-private void writeToCockpit(int displayNum, string text, int cockPitNum=0) {
-    List<IMyCockpit> blocks = new List<IMyCockpit>();
-    GridTerminalSystem.GetBlocksOfType<IMyCockpit>(blocks);
-    if (blocks.Count < cockPitNum) {
-        Echo("No Cockpit with ID: " + cockPitNum);
-        return;
-    }
-
-    IMyTextSurfaceProvider c = blocks[cockPitNum] as IMyCockpit;
-    if (c.SurfaceCount < displayNum) {
-        Echo("No Display with ID: " + displayNum);
-        return;
-    }
-
-    c.GetSurface(displayNum).WriteText(text);
-}
-
-private void setupCockpit(int displayNum, float fontSize, Color fontColor, int cockPitNum = 0) {
-   List<IMyCockpit> blocks = new List<IMyCockpit>();
-    GridTerminalSystem.GetBlocksOfType<IMyCockpit>(blocks);
-    if (blocks.Count < cockPitNum) {
-        Echo("No Cockpit with ID: " + cockPitNum);
-        return;
-    }
-
-    IMyTextSurfaceProvider c = blocks[cockPitNum] as IMyCockpit;
-    if (c.SurfaceCount < displayNum) {
-        Echo("No Display with ID: " + displayNum);
-        return;
-    }
-    
-   IMyTextSurface display = c.GetSurface(displayNum);
-    display.ContentType = ContentType.TEXT_AND_IMAGE;
-    display.FontColor = fontColor;
-    display.FontSize = fontSize;
-}
 // YOUR CODE END
 #region PreludeFooter
     }
---
+++ Scripts/CargoD Script/Script.cs
@@ -0,0 +1,93 @@
+#region Prelude
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+using VRageMath;
+using VRage.Game;
+using VRage.Collections;
+using Sandbox.ModAPI.Ingame;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.GUI.TextPanel;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.Game.EntityComponents;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+
+namespace SpaceEngineers.UWBlockPrograms.CargoDScript {
+    public sealed class Program : MyGridProgram {
+#endregion
+// YOUR CODE BEGIN
+﻿public void Main(string argument, UpdateType updateSource) {
+    if (argument == "") {
+       argument = "0";
+   }
+    setupCockpit(Convert.ToInt32(argument),  1.5f, Color.Orange);
+
+    List<IMyFunctionalBlock> blocks = new List<IMyFunctionalBlock>();
+    GridTerminalSystem.GetBlocksOfType<IMyFunctionalBlock>(blocks);
+
+    float tempVal;
+    string printString = "";
+    float maxInv = 0;
+    float curInv = 0;
+    foreach (IMyFunctionalBlock block in blocks) {
+        if (!block.HasInventory || block is IMyReactor || !block.ShowInInventory) continue;
+
+        IMyInventory inv = block.GetInventory();
+        maxInv += (float)inv.MaxVolume;
+        curInv += (float)inv.CurrentVolume;
+        tempVal = (float)inv.CurrentVolume * 100 / (float)inv.MaxVolume;
+        printString += block.CustomName + ": " + Convert.ToInt32(tempVal) + "%\n";
+    }
+
+   int fillRatio = (int)(curInv * 100 / maxInv);
+   printString = "Fill Ratio: " + fillRatio + "%\n" + "----------\n" + printString;
+   writeToCockpit(Convert.ToInt32(argument), printString);
+   Runtime.UpdateFrequency = UpdateFrequency.Update100;
+}
+
+private void writeToCockpit(int displayNum, string text, int cockPitNum=0) {
+    List<IMyCockpit> blocks = new List<IMyCockpit>();
+    GridTerminalSystem.GetBlocksOfType<IMyCockpit>(blocks);
+    if (blocks.Count < cockPitNum) {
+        Echo("No Cockpit with ID: " + cockPitNum);
+        return;
+    }
+
+    IMyTextSurfaceProvider c = blocks[cockPitNum] as IMyCockpit;
+    if (c.SurfaceCount < displayNum) {
+        Echo("No Display with ID: " + displayNum);
+        return;
+    }
+
+    c.GetSurface(displayNum).WriteText(text);
+}
+
+private void setupCockpit(int displayNum, float fontSize, Color fontColor, int cockPitNum = 0) {
+   List<IMyCockpit> blocks = new List<IMyCockpit>();
+    GridTerminalSystem.GetBlocksOfType<IMyCockpit>(blocks);
+    if (blocks.Count < cockPitNum) {
+        Echo("No Cockpit with ID: " + cockPitNum);
+        return;
+    }
+
+    IMyTextSurfaceProvider c = blocks[cockPitNum] as IMyCockpit;
+    if (c.SurfaceCount < displayNum) {
+        Echo("No Display with ID: " + displayNum);
+        return;
+    }
+    
+   IMyTextSurface display = c.GetSurface(displayNum);
+    display.ContentType = ContentType.TEXT_AND_IMAGE;
+    display.FontColor = fontColor;
+    display.FontSize = fontSize;
+}
+// YOUR CODE END
+#region PreludeFooter
+    }
+}
+#endregion
